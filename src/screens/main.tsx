57e79a5308010df51810a60513ef63276f2c9c05
Shahar <shaharm22@gmail.com>
2024-07-07 03:37:55 +0300

    init

---
+++ src/screens/main.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+function main() {
+    return (
+        <View>
+            <Text>
+                ddsvsd
+            </Text>
+        </View>
+    )
+}
+
+
+
+const styles = StyleSheet.create({
+    generalDesign: {
+        backgroundColor: "#D6EFD8",
+        flex: 1
+    }
+});
+
+
+export default main
